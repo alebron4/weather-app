9cf5d21eb719db055add825ac4a4e8d8e9be0791
alebron4 <101158351+alebron4@users.noreply.github.com>
2022-05-22 16:07:31 -0400

    Added API for forecast in index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Mon", "Tues", "Wed"];
@@ -46,7 +47,11 @@ function showForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
+function getForecast(coordinates) {
+  let apiKey = "fe62e0fa78e8a82e333a6473b76e27aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
 function showTemp(response) {
   celsiusTemperature = response.data.main.temp;
   let currentTemp = document.querySelector("#temperature");
@@ -67,6 +72,8 @@ function showTemp(response) {
   iconElement.setAttribute("alt", response.data.weather[0].main);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -108,4 +115,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 search("Barcelona");
-showForecast();
